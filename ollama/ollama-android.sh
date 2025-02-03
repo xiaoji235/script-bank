4f86908de8d3a293239812b27345d10fd603768c
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-02-03 13:09:33 +0800

    Update ollama-android.sh

--- ollama/ollama-android.sh
+++ ollama/ollama-android.sh
@@ -44,6 +44,7 @@ sleep 1
 cp $PREFIX/etc/termux-login.sh $PREFIX/etc/termux-login.sh.bak
 echo "echo \"正在启动 ollama 服务...\"" >> $PREFIX/etc/termux-login.sh
 exho "ollama serve" >> $PREFIX/etc/termux-login.sh
+echo "正在启动 ollama 服务..."
 ollama serve
 echo "正在部署 deepseek-r1:1.5b 模型..."
 ollama run deepseek-r1:1.5b
