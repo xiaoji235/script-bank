94370a5005525410a2d34061f1264f144654c36b
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-02-03 16:00:41 +0800

    Update ollama-android.sh

--- ollama/ollama-android.sh
+++ ollama/ollama-android.sh
@@ -41,9 +41,12 @@ repeat_until_success rm -rf ollama
 echo "ollama 安装成功"
 
 sleep 1
+cp $PREFIX/etc/motd $PREFIX/etc/motdbak
+wget https://cdn.jsdelivr.net/gh/xiaoji235/script-bank/ollama/motd $PREFIX/etc/motd
 cp $PREFIX/etc/termux-login.sh $PREFIX/etc/termux-login.sh.bak
 echo "echo \"正在启动 ollama 服务...\"" >> $PREFIX/etc/termux-login.sh
 echo "ollama serve" >> $PREFIX/etc/termux-login.sh
+echo "ollama run deepseek-r1:1.5b" >> $PREFIX/etc/termux-login.sh
 echo "正在启动 ollama 服务..."
 ollama serve
 echo "正在部署 deepseek-r1:1.5b 模型..."
