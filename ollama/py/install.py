77a7f3a2ac886c97509d85bc5ed6bd4a5983131f
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-02-06 12:47:25 +0800

    Update install.py

--- ollama/py/install.py
+++ ollama/py/install.py
@@ -4,5 +4,5 @@ url = 'https://api.github.com/repos/ollama/ollama/releases'
 response = requests.get(url)
 data = response.json()
 
-first_tag_name = data[0]['tag_name']
-print(first_tag_name)
+name = data[0]['name']
+print(name)
