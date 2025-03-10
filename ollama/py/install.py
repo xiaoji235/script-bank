c1574070899cb9f54321c0e60429f1595b85ef51
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-03-10 22:28:06 +0800

    Update install.py

--- ollama/py/install.py
+++ ollama/py/install.py
@@ -1,8 +1,17 @@
 import requests
 
-url = 'https://api.github.com/repos/ollama/ollama/releases'
-response = requests.get(url)
-data = response.json()
-
-name = data[0]['name']
-print(name)
+url = "https://api.github.com/repos/ollama/ollama/releases"
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # 检查请求是否成功
+    releases = response.json()
+    if releases:  # 检查是否有 release 数据
+        first_tag_name = releases[0].get('tag_name')  # 获取最新版本
+        if first_tag_name:
+            print(first_tag_name)
+        else:
+            print("v0.5.13") #如果没有获取到最新版本则用保底版本
+    else:
+        print("v0.5.13") #如果没有获取到最新版本则用保底版本
+except requests.exceptions.RequestException as e:
+    print(f"请求失败: {e}")
